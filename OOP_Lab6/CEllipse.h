08ee74b32f9142bb3b50b094dacd6edecd213568
Nezarik-Intmax <nezarikintmax@gmail.com>
2022-01-06 01:51:38 +0500

    Final?

--- OOP_Lab6/CEllipse.h
+++ OOP_Lab6/CEllipse.h
@@ -32,9 +32,15 @@ public:
 			e->Graphics->DrawEllipse(gcnew Pen(Brushes::Red, 10), x, y, width, height);
 		e->Graphics->FillEllipse(brsh, x, y, width, height);
 	}
-	virtual bool checkCollision(int x, int y) override{
-		if((this->x <= x) && (this->x2 >= x))
-			if((this->y <= y) && (this->y2 >= y))
+	virtual bool checkCollision(int xC, int yC) override{
+		double a = width/2;
+		double b = height/2;
+		double xCent = (x + (x2 - x)/2);
+		double yCent = (y + (y2 - y)/2);
+		double xP = xC - xCent;
+		double yP = yC - yCent;
+		if(a!=0&&b!=0)
+			if((((xP*xP)/(a*a)) + ((yP*yP)/(b*b))) <= 1)
 				return true;
 		return false;
 	}
